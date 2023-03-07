d8bdc8aae5244ed99f109b28f50e5a5dbb1bfade
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-03-07 17:09:56 +0100

    add index

--- src/main/java/com/example/CRUD/CrudApplication.java
+++ src/main/java/com/example/CRUD/CrudApplication.java
@@ -1,22 +1,27 @@
 package com.example.CRUD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @SpringBootApplication
-@RestController
+@Controller
 public class CrudApplication {
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "every body") String name) {
-		return String.format("Hello  peopleee %s!", name);
-	}
-	@GetMapping("/createtask")
-	public String create(@RequestParam(value = "task", defaultValue = "pan")String name){
-		return String.format("comprar %s!", name);
-	}
+		@GetMapping("/")
+		public String inicio () {
+			return "index";
+		}
+
+
+
+
 }
