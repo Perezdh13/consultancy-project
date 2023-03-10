c1ae5a836f51d351b90676b567c492e14c4634eb
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-03-10 12:04:03 +0100

    create add to form

--- src/main/java/com/example/CRUD/controller/CConsultationController.java
+++ src/main/java/com/example/CRUD/controller/CConsultationController.java
@@ -6,18 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
-@RequestMapping
+
 public class CConsultationController {
   @Autowired
     private IConsultation iConsultation;
 
     @GetMapping("/")
-    public String list (Model model) { List<CConsultation> consultation = (List<CConsultation>) iConsultation.findAll();
+    public String list (Model model) {
+        List<CConsultation> consultation = (List<CConsultation>) iConsultation.findAll();
     return "index";
+    }
+    @GetMapping("/form")
+  public String addConsult(Model model){
+      model.addAttribute("consult",new CConsultation());
+      return "form";
+    }
+    @PostMapping("/form")
+    public String saveConsult(@ModelAttribute CConsultation consult, RedirectAttributes redirectAttrs) {
+        iConsultation.save(consult);
+        redirectAttrs
+                .addFlashAttribute("mensaje", "Agregado correctamente")
+                .addFlashAttribute("clase", "success");
+        return "redirect:/";
     }
  }
--- src/main/java/com/example/CRUD/model/CConsultation.java
+++ src/main/java/com/example/CRUD/model/CConsultation.java
@@ -12,51 +12,14 @@ public class CConsultation  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column
-    private String name;
-    private String surname;
-    private String title;
-    private String description;
-    /*private String getDateTime(){
-        DateFormat dateFormat = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
-        Date date = new Date();
-        return  dateFormat.format(date);
-    }*/
-    public CConsultation(){
 
+    public String getUser() {
+        return user;
     }
 
-    public CConsultation (int id, String name, String surname, String title, String description) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.title = title;
-        this.description = description;
-
+    public void setUser(String user) {
+        this.user = user;
     }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
 
     public String getTitle() {
         return title;
@@ -73,4 +36,25 @@ public class CConsultation  {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    private String user;
+    private String title;
+    private String description;
+    /*private String getDateTime(){
+        DateFormat dateFormat = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return  dateFormat.format(date);
+    }*/
+    public CConsultation(){
+
+    }
+
+    public CConsultation (int id, String user, String title, String description) {
+        this.id = id;
+        this.user = user;
+        this.title = title;
+        this.description = description;
+
+    }
+
 }
