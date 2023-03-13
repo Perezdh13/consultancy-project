f581cb6bbecc978f47e1a6a5121ba4b827e3931f
Raul Villar <raultheviking@gmail.com>
2023-03-13 12:47:32 +0100

    Merge branch 'form' of https://github.com/Perezdh13/consultancy-project into header
    # Conflicts:
    #	src/main/java/com/example/CRUD/controller/CConsultationController.java
    #	src/main/java/com/example/CRUD/interfaces/IConsultation.java
    #	src/main/java/com/example/CRUD/model/CConsultation.java
    #	src/main/resources/application.properties

--- src/main/java/com/example/CRUD/controller/CConsultationController.java
+++ src/main/java/com/example/CRUD/controller/CConsultationController.java
@@ -3,15 +3,14 @@ package com.example.CRUD.controller;
 import com.example.CRUD.interfaces.IConsultation;
 import com.example.CRUD.model.CConsultation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import java.util.Optional;
 
 @Controller
 
