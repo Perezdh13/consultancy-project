3530b5f0243c8ce57011006f1295a59d19e97b46
Raul Villar <raultheviking@gmail.com>
2023-03-14 09:10:56 +0100

    Merge branch 'form' of https://github.com/Perezdh13/consultancy-project into header
    # Conflicts:
    #	src/main/java/com/example/CRUD/controller/CConsultationController.java
    #	src/main/java/com/example/CRUD/interfaces/IConsultation.java
    #	src/main/java/com/example/CRUD/model/CConsultation.java
    #	src/main/resources/application.properties

--- src/test/java/com/example/CRUD/CrudApplicationTests.java
+++ src/test/java/com/example/CRUD/CrudApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.CRUD;
 
+import com.example.CRUD.model.CConsultation;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
