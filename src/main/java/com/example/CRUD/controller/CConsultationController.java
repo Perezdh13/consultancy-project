6f750750d40e474acaf019cad0cf7f037a411329
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-03-15 11:27:46 +0100

    date

--- src/main/java/com/example/CRUD/controller/CConsultationController.java
+++ src/main/java/com/example/CRUD/controller/CConsultationController.java
@@ -1,13 +1,13 @@
 package com.example.CRUD.controller;
-
 import com.example.CRUD.interfaces.IConsultation;
 import com.example.CRUD.model.CConsultation;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Controller
@@ -34,14 +34,22 @@ public class CConsultationController {
         return "redirect:/";
     }
     @GetMapping("/edit/{id}")
-    public String showEditForm(@PathVariable("id") int id, Model model) {
+    public String showEditForm(@PathVariable("id") int id, @NotNull Model model) {
         Optional<CConsultation> consultation = iConsultation.findById(id);
         model.addAttribute("consultation", consultation);
         return "edit";
     }
     @PostMapping("/edit/{id}")
     public String updateConsultation(@PathVariable("id") int id, @ModelAttribute("consultation") CConsultation consultation) {
-        iConsultation.save(consultation);
+        Optional<CConsultation> updateConsultation = iConsultation.findById(id);
+        updateConsultation.ifPresent(c -> {
+            c.setName(consultation.getName());
+            c.setStack(consultation.getStack());
+            c.setTitle(consultation.getTitle());
+            c.setDescription(consultation.getDescription());
+            c.setDate(LocalDate.now());
+            iConsultation.save(c);
+        });
         return "redirect:/";
     }
     @GetMapping("/delete/{id}")
